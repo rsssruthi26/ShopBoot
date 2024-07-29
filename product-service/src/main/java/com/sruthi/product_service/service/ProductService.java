656032c1cf8f0094f1face00d59949a19ee95e6c
Sai Satya Sruthi <rsssruthi26@gmail.com>
2024-07-29 17:14:02 -0400

    Product Service

---
+++ product-service/src/main/java/com/sruthi/product_service/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.sruthi.product_service.service;
+
+import com.sruthi.product_service.dto.ProductRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void createProduct(ProductRequest productRequest) {
+        // map the productRequest to the productModel
+    }
+}
